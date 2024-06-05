bdae312e07836f4adeed43cd7804d21bcece95e8
PauAb42 <pau.aleb@hotmail.com>
2024-06-05 11:35:24 -0600

    ejemplos

--- parcial2/pythonMath.py
+++ parcial2/pythonMath.py
@@ -1,1 +1,74 @@
-# .:: Python Math ::. 
+#.:: Python Math ::.
+
+# ..::: FUNCIONES MATEMÁTICAS INTEGRADAS :::..
+
+#1. min() y max(). Se pueden usar para encontrar el valor más bajo o más alto en un iterable
+
+# x=min(25,46,-85)
+# y=max(25,46,85)
+
+# print(x)
+# print(y)
+
+#2. abs()Es una funcion que devuelve el valor absoluto (positivo) del número especificado.
+
+# x=abs(-95.46)
+# print(x)
+
+#3. pow(x,y) Duvuelve el resultado de elevar un número a una potencia
+
+# x=pow(4,3)
+# print(x)
+#4. divmod() Toma dos números y devuelve una tupla que contiene el cociente y el residuo.
+
+# division=divmod(25,4)
+# print(division)
+
+# cociente, residuo=divmod(25,4)
+# print(cociente)
+# print(residuo)
+
+#5. round() Redondea un número flotante al entero más cercano o al número de decimales especificado.
+
+# numero=5.64854965
+# redondeado=round(numero,2)
+# print(redondeado)
+
+
+
+#6. sum() Suma los elementos de un iterable (como una lista o una tupla)
+
+
+# numeros=[1,2,3,4,5]
+# suma=sum(numeros)
+# print(suma)
+
+
+
+# ..::: FUNCIONES DE IMPORT MATH :::..
+
+# import math
+
+#1. math.sqrt() devuelve la raíz cuadrada de un número
+
+# import math
+# x=math.sqrt(64)
+# print(x)
+
+#2. math.ceil() redondea un número hacia arriba a su entero más cercano y el math.floor() redondea un número hacia abajo a su entero más cercano y devuelve el resultado
+
+# import math
+# x=math.ceil(1.6)
+# y=math.floor(1.6)
+
+# print(x)
+# print(y)
+
+
+#3. math.pi es una constante que devuelve el valor de PI
+
+# import math
+
+# x=math.pi
+# print(x)
+
